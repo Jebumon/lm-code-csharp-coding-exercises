30070dfae9d3d47cd1cd2b4ea0021525e9e1b26d
jebumon <jebukthomas@gmail.com>
2022-08-30 19:28:48 +0100

    Exercise005: Code refactor done using lambda expression.

--- Exercises/Exercise005.cs
+++ Exercises/Exercise005.cs
@@ -5,9 +5,6 @@ namespace Exercises
 {
     public class Exercise005
     {
-        public bool IsPangram(string input)
-        {
-            return input.ToLower().Where(elements => Char.IsLetter(elements)).GroupBy(elements => elements).Count() == 26;
-        }
+        public bool IsPangram(string input) => input.ToLower().Where(elements => Char.IsLetter(elements)).GroupBy(elements => elements).Count() == 26;
     }
 }
