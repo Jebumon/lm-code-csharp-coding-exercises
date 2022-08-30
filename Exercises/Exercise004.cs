b8f4cd5e01b154294a21dce932b4c2e184270f65
jebumon <jebukthomas@gmail.com>
2022-08-30 19:26:38 +0100

    Exercise004 : Code refactor done using lambda expression.

--- Exercises/Exercise004.cs
+++ Exercises/Exercise004.cs
@@ -3,9 +3,6 @@ namespace Exercises
 {
     public class Exercise004
     {
-        public static DateTime AddGigaSecond(DateTime dateTime)
-        {
-            return dateTime.AddSeconds(1000000000);
-        }
+        public static DateTime AddGigaSecond(DateTime dateTime) => dateTime.AddSeconds(1000000000);
     }
 }
