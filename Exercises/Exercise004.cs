f883d158a70b57e1f1df8fec4a92b11f6fc35eb1
jebumon <jebukthomas@gmail.com>
2022-08-31 09:10:25 +0100

    Exercise004 : Implimented 1 giga second as "Magic number" (ONE_GIGA_SECOND)

--- Exercises/Exercise004.cs
+++ Exercises/Exercise004.cs
@@ -3,6 +3,7 @@ namespace Exercises
 {
     public class Exercise004
     {
-        public static DateTime AddGigaSecond(DateTime dateTime) => dateTime.AddSeconds(1000000000);
+        public static int ONE_GIGA_SECOND = 1000000000;
+        public static DateTime AddGigaSecond(DateTime dateTime) => dateTime.AddSeconds(ONE_GIGA_SECOND);
     }
 }
