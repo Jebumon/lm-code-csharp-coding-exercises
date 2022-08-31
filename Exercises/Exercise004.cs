feb58543881edc08d5c751cea4f4228614738590
jebumon <jebukthomas@gmail.com>
2022-08-31 09:15:14 +0100

    Magic number variable modified.

--- Exercises/Exercise004.cs
+++ Exercises/Exercise004.cs
@@ -3,7 +3,7 @@ namespace Exercises
 {
     public class Exercise004
     {
-        public static int ONE_GIGA_SECOND = 1000000000;
-        public static DateTime AddGigaSecond(DateTime dateTime) => dateTime.AddSeconds(ONE_GIGA_SECOND);
+        public static int ONE_GIGASECOND = 1000000000;
+        public static DateTime AddGigaSecond(DateTime dateTime) => dateTime.AddSeconds(ONE_GIGASECOND);
     }
 }
